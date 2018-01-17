87728132cdce15046871de9c20f28266d3478ada
Lior Rozen <lixroz+github@gmail.com>
2018-01-17 13:07:33 +0200

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="panoply_postgres",
-    version="2.1.0",
+    version="2.2.0",
     description="Panoply Data Source for Postgres",
     author="Panoply Dev Team",
     author_email="support@panoply.io",
