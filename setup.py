abbcfa810f566fffa0ed893c518b5fd1fc089769
Daniel Lizardo <daniel.lizardo@narrasoft.com>
2019-05-27 19:01:11 +0800

    bump version, update sdk, codestyle package

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@ from distutils.core import setup
 
 setup(
     name="panoply_postgres",
-    version="2.2.2",
+    version="2.3.2",
     description="Panoply Data Source for Postgres",
     author="Panoply Dev Team",
     author_email="support@panoply.io",
     url="http://panoply.io",
     install_requires=[
-        "panoply-python-sdk==1.4.0",
+        "panoply-python-sdk==1.6.0",
         "psycopg2==2.7.1",
         "backoff==1.4.3"
     ],
     extras_require={
         "test": [
-            "pep8==1.7.0",
+            "pycodestyle==2.5.0",
             "coverage==4.3.4",
             "mock==2.0.0"
         ]
