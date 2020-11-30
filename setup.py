06dd620a645586fcb461906320c32dd28ecf1821
solomiya-hatalyak <solomiya@panoply.io>
2020-11-30 16:53:10 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ required_packages = [
 
 setup(
     name="panoply_postgres",
-    version="3.0.2",
+    version="3.0.3",
     description="Panoply Data Source for Postgres",
     author="Panoply Dev Team",
     author_email="support@panoply.io",
